22dbac003e93541e5de041d2b895c662b0ccbfbc
Prince <ankitsharma0004d@gmail.com>
2024-09-05 11:53:18 +0530

    Added node-cron to schedule the task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const { sendBasicEmail } = require('./services/email-service')
+const cron = require('node-cron');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -12,12 +13,17 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server Started at PORT ${PORT}`);
 
-        sendBasicEmail(
-            'Support <support@admin.com>',
-            'ankitsharma0004d@gmail.com',
-            'This is a testing mail',
-            'Hey, how are you, you would have liked our support'
-        );
+        // sendBasicEmail(
+        //     'Support <support@admin.com>',
+        //     'ankitsharma0004d@gmail.com',
+        //     'This is a testing mail',
+        //     'Hey, how are you, you would have liked our support'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every 2 minutes');
+            
+        })
         
     })
 }
