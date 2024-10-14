7757ff0d25e18be15a32d28ad9891192569015e3
Prince <ankitsharma0004d@gmail.com>
2024-10-15 02:27:40 +0530

    Added RabbitMQ config for Reminder Service

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service')
+// const { sendBasicEmail } = require('./services/email-service')
+
+const { createChannel } = require('./utils/messageQueue');
 
 const jobs  = require('./utils/job')
 const TicektController = require('./contrllers/ticket-controller');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
